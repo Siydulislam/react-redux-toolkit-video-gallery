80752ca41eba6d7e0a38d4f4986819ca57a1c498
Siydulislam <siydulislam333@gmail.com>
2022-09-21 17:46:01 +0600

    filter and search slice, API and UI integration

--- src/components/navbar/Search.js
+++ src/components/navbar/Search.js
@@ -1,11 +1,26 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import { searched } from "../../features/filter/filterSlice";
+
 export default function Search() {
+    const dispatch = useDispatch();
+    const { search } = useSelector(state => state.filter);
+    const [input, setInput] = useState(search);
+
+    const handlaSubmit = (e) => {
+        e.preventDefault();
+        dispatch(searched(input));
+    }
+
     return (
-        <form>
+        <form onSubmit={handlaSubmit}>
             <input
                 className="outline-none border-none mr-2"
                 type="search"
                 name="search"
                 placeholder="Search"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
             />
         </form>
     )
