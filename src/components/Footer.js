5c54aacdb12a56863e840327d7e21a28fcc90bbc
Siydulislam <siydulislam333@gmail.com>
2022-09-21 19:30:33 +0600

    change footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,14 @@ export default function Footer() {
             <div
                 className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex justify-between gap-2 border-t text-sm text-slate-400"
             >
-                <div>Copyright 2022 Learn with Sumit.</div>
+                <div>Copyright 2022</div>
                 <div>
                     <a
-                        href="https://youtube.com/learnwithsumit"
+                        href="https://www.linkedin.com/in/siydul-islam-187b88157/"
                         target="_blank"
                         rel="noreferrer"
                     >
-                        YouTube Channel
+                        LinkedIn
                     </a>
                 </div>
             </div>
