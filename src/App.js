9f850102fbe8bedf71769ee9f9e8c9f19b45b522
Siydulislam <siydulislam333@gmail.com>
2023-03-06 13:28:17 +0600

    design the ui components in react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Home from "./pages/Home";
+// import Video from "./pages/Video";
+
 function App() {
   return (
-    <div>
-      Hello World!
-    </div>
+    <>
+      <Home />
+      {/* <Video /> */}
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+export default function Footer() {
+    return (
+        <section className="pt-6">
+            <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex justify-between gap-2 border-t text-sm text-slate-400">
+                <div>Copyright 2023</div>
+                <div>
+                    <a href="https://youtube.com/learnwithsumit" target="_blank" rel="noreferrer">
+                        Credit goes to LWS
+                    </a>
+                </div>
+            </div>
+        </section>
+    )
+}
