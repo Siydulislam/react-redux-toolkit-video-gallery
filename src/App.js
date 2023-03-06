efab4253a7430f5e01dece976c26e67fa47bc713
Siydulislam <siydulislam333@gmail.com>
2023-03-06 13:51:23 +0600

    setup routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import Footer from "./components/Footer";
+import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/Home";
-// import Video from "./pages/Video";
+import Video from "./pages/Video";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Home />
-      {/* <Video /> */}
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/videos/:videoId" element={<Video />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
